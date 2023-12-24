3d84c00f6cd226b731de67b7bbe5d39f68d21905
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-24 15:17:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+// NO.1
+
+
+
+function writeCards ( arr, occasion ){
+    let messageArr = [];
+    let message;
+    for ( let i = 0; i < arr.length; i++ ){
+        message = `Thank you, ${ arr[i] }, for the wonderful ${ occasion } gift!`;
+        messageArr.push( message );
+        //console.log ( message );
+        
+    }
+    return messageArr;
+}
+
+let thankYouMessages = writeCards ( ["Guadalupe", "Ollie", "Aki"], "surprise"  );
+console.log ( thankYouMessages );
+
+//NO. 2
+
+function countDown ( num1 ){
+    while ( num1 >= 0 ){
+        console.log ( num1-- );
+    }
+}
+
+countDown ( 12 );
